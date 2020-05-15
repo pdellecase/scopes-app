f485bc0f786ac979834b3d84ffba931724877f5d
pdellecase <dev@dellecase.com>
2020-05-15 18:58:23 +0200

    fixing user list API

--- user-list.js
+++ user-list.js
@@ -3,17 +3,15 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: process.env.usersTableName
+    TableName: process.env.usersTableName,
     // 'KeyConditionExpression' defines the condition for the query
     // - 'userId = :userId': only return items with matching 'userId'
     //   partition key
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
-    // KeyConditionExpression: "userId = :userId",
-    // ExpressionAttributeValues: {
-    //   ":userId": event.requestContext.identity.cognitoIdentityId
-    // }
+     KeyConditionExpression: "",
+     ExpressionAttributeValues: {}
   };
 
   try {
