36e8114f2c59ffa577305fcba9e24c2b2c0ff696
pdellecase <dev@dellecase.com>
2020-05-15 19:19:51 +0200

    fixing user list API

--- user-list.js
+++ user-list.js
@@ -10,8 +10,7 @@ export async function main(event, context) {
     // 'ExpressionAttributeValues' defines the value in the condition
     // - ':userId': defines 'userId' to be Identity Pool identity id
     //   of the authenticated user
-     KeyConditionExpression: "",
-     ExpressionAttributeValues: {}
+     KeyConditionExpression: "*"
   };
 
   try {
