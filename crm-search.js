32bbd573630efe665395e2aa82a336e0c5d12e7b
pdellecase <dev@dellecase.com>
2020-05-31 21:30:18 +0200

    adding CRM Search API

--- crm-search.js
+++ crm-search.js
@@ -2,26 +2,25 @@
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
-  
-   // Load CRM Middleware URL and Token from the  environment variables
+
+  // Load CRM Middleware URL and Token from the  environment variables
   const crmMiddlewareUrl = process.env.crmMiddewareUrl;
   const crmMiddlewareToken = process.env.crmMiddlewareToken;
 
   // Get parameters from API Call
-  
-  const clientKey = event.pathParameters.client-key;
-  const searchString = event.pathParameters.search-string;
-  const servicesOnly = event.pathParameters.services-only;
-  const scopingContact = event.pathParameters.scoping-contact;
 
-  
-  
+  const clientKey = event.pathParameters.clientKey;
+  const searchString = event.pathParameters.searchString;
+  const servicesOnly = event.pathParameters.servicesOnly;
+  const scopingContact = event.pathParameters.scopingContact;
 
   try {
+    console.log("CRM-SEARCH Received:crmMiddlewareUrl=" + crmMiddlewareUrl +", crmMiddlewareToken=" + crmMiddlewareToken );
     console.log("CRM-SEARCH Received:clientKey=" + clientKey +", searchString=" + searchString + ", servicesOnly=" + servicesOnly + ", scopingContact=" + scopingContact );
     //const result = await dynamoDbLib.call("scan", params);
     // Return the matching list of items in response body
     //return success(result.Items);
+    return success("");
   } catch (e) {
     console.log(e);
     return failure({ status: false });
