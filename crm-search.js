f8217b47df3b64b9bfc95ebe728018f4b20d5ca4
pdellecase <dev@dellecase.com>
2020-06-10 22:29:58 +0200

    Fixed CRM-Search API access

--- crm-search.js
+++ crm-search.js
@@ -1,9 +1,8 @@
-
 import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
 
-  console.log("CRM-SEARCH starting ...");
+  console.log("CRM-SEARCH initiated ...");
   // Load CRM Middleware URL and Token from the  environment variables
   const crmMiddlewareUrl = process.env.crmMiddewareUrl;
   const crmMiddlewareToken = process.env.crmMiddlewareToken;
@@ -11,81 +10,83 @@ export async function main(event, context) {
   // Get parameters from API Call
 
   const clientKey = event.pathParameters.clientKey;
+  const id = event.pathParameters.id;
+  var full = event.pathParameters.full;
   const searchString = event.pathParameters.searchString;
-  const servicesOnly = event.pathParameters.servicesOnly;
+  var servicesOnly = event.pathParameters.servicesOnly;
   const scopingContact = event.pathParameters.scopingContact;
 
-  var https = require('https');
-  let dataString = '';
+  // ** Build the API path based on the parameter provided **
   let crmMiddlewarePath = '';
-  if(scopingContact ==='null'){
-    crmMiddlewarePath = "/?client-key=" + clientKey + "&search-string=" + searchString + "&services-only=" + servicesOnly;
-  } else {
-    crmMiddlewarePath = "/?client-key=" + clientKey + "&scoping-contact=" + scopingContact;
+
+  if((clientKey === null)||(clientKey ===undefined)||(clientKey === 'null')){
+    return failure({ error: "No Client Key provided" });
   }
+  // search use case
+  if((id === null)||(id ===undefined)||(id === 'null')){
+    if((scopingContact === null)||(scopingContact ===undefined)||(scopingContact === 'null')){
+      if((searchString === null)||(searchString ===undefined)||(searchString === 'null')) { return failure({ error: "No Search String provided" }); }
+      if((servicesOnly === null)||(servicesOnly ===undefined)||(servicesOnly === 'null')) { servicesOnly='no'; }
+      crmMiddlewarePath = "/?client-key=" + clientKey + "&search-string=" + searchString + "&services-only=" + servicesOnly;
+    } else {
+      crmMiddlewarePath = "/?client-key=" + clientKey + "&scoping-contact=" + scopingContact;
+    }
+  }
+  // get use case
+  else {
+    if((full === null)||(full ===undefined)||(full === 'null')) { full='no'; }
+    crmMiddlewarePath = "/?client-key=" + clientKey + "&id=" + id + "&full=" + full;
+  }
+
   console.log("CRM-SEARCH crmMiddlewareUrl = " + crmMiddlewareUrl);
   console.log("CRM-SEARCH crmMiddlewarePath = " + crmMiddlewarePath);
-  var params = {
-    host: crmMiddlewareUrl,
-    path: crmMiddlewarePath,
-    method: 'GET',
-    rejectUnauthorized: false,
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-               'x-csrf-token': crmMiddlewareToken}
-  };
 
-  const response = await new Promise((resolve, reject) => {
-    const req = https.request(params, function(res) {
-      res.on('data', chunk => {
-        dataString += chunk;
-      });
-      res.on('end', () => {
-        console.log("Success >> " + JSON.stringify(JSON.parse(dataString)));
-        return success(JSON.parse(dataString));
-      });
-    });
-    req.on('error', (e) => {
-      console.log("Failure >> " + e.message);
-      return failure({ error: e.message });
-    });
+  const http = require('https');
+
+  return httprequest().then((data) => {
+    console.log("CRM-SEARCH Success: statusCode 200");
+    return success(data);
   });
-  return response;
-/**
-  try {
-    console.log("CRM-SEARCH Received:crmMiddlewareUrl=" + crmMiddlewareUrl +", crmMiddlewareToken=" + crmMiddlewareToken );
-    console.log("CRM-SEARCH Received:clientKey=" + clientKey +", searchString=" + searchString + ", servicesOnly=" + servicesOnly + ", scopingContact=" + scopingContact );
 
-    var https = require('https');
-    console.log("YO 1");
-      var params = {
-                    host: crmMiddlewareUrl,
-                    path: "?client-key=" + clientKey + "&search-string=" + searchString + "&services-only" + servicesOnly,
-                    method: 'GET',
-                    headers: { 'Content-Type': 'application/x-www-form-urlencoded',
-                               'x-csrf-token': 'PYGLj6gMaQCZ3r7Ur5OrPgp3ePvzKzCD' },
-                   };
 
-      var req = https.request(params, function(res) {
-        console.log("YO 2");
-        let data = '';
-        console.log('STATUS: ' + res.statusCode);
-        res.setEncoding('utf8');
+  function httprequest() {
+    return new Promise((resolve, reject) => {
+      const options = {
+          host: crmMiddlewareUrl,
+          path: crmMiddlewarePath,
+          port: 443,
+          method: 'GET',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded',
+               'x-csrf-token': crmMiddlewareToken}
+      };
+      const req = http.request(options, (res) => {
+        if (res.statusCode < 200 || res.statusCode >= 300) {
+          console.log("CRM Http Request Error = " + res.statusCode);
+          //return reject(new Error('statusCode=' + res.statusCode));
+          return reject(failure({ error: res.statusCode }));
+        }
+        var body = [];
         res.on('data', function(chunk) {
-          data += chunk;
+          body.push(chunk);
         });
         res.on('end', function() {
-        console.log("DONE");
-        console.log(JSON.parse(data));
+          try {
+            body = JSON.parse(Buffer.concat(body).toString());
+          } catch(e) {
+            console.log("Error = " + e.message);
+            reject(e);
+          }
+          resolve(body);
         });
       });
+
+      req.on('error', (e) => {
+        console.log("Error = " + e.message);
+        return failure({ error: e.message });
+      });
+      // send the request
       req.end();
-    //const result = await dynamoDbLib.call("scan", params);
-    // Return the matching list of items in response body
-    //return success(result.Items);
-    return success("");
-  } catch (e) {
-    console.log(e);
-    return failure({ status: false });
+    });
   }
-  */
+
 }
